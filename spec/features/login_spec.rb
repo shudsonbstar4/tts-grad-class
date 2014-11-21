a34ec5a356e2f71cc6b27e2b1972e32a76c5807d
Jim Van Fleet <jim@jimvanfleet.com>
2014-11-20 19:12:42 -0500

    Logging in shows email

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -15,4 +15,16 @@ describe "Logging in", :type => :feature do
 
     expect(page).to have_content 'Signed in successfully.'
   end
+  
+  it "shows me my email in the navigation" do
+    visit new_user_session_path
+    within("#new_user") do
+      fill_in 'Email', :with => user.email
+      fill_in 'Password', :with => '12345678'
+      click_button 'Log in'
+    end
+    #save_and_open_page
+
+    expect(page).to have_content user.email
+  end
 end
